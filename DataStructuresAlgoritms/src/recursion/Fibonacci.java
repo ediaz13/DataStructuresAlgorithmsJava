fec31010dc25a0e0c97568267e24aa7168640a7f
ediaz13 <diazemanuel27@gmail.com>
2023-01-02 22:54:03 -0300

    First Commit Recursion

---
+++ DataStructuresAlgoritms/src/recursion/Fibonacci.java
@@ -0,0 +1,23 @@
+package recursion;
+
+class Fibonacci {
+  public static void main(String[] args) {
+    Fibonacci recursion = new Fibonacci();
+    var rec = recursion.fibonacci(-2);
+    System.out.println(rec);
+
+  }
+
+  public int fibonacci(int n) {
+    if (n<0) {
+      return -1;
+    }
+    if (n==0 || n==1) {
+      return n;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
+  }
+
+
+
+}
